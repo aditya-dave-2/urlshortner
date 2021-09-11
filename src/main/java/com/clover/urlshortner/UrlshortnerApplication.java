e6c07ed6a912f73fb532884bf30a92e90e0e38ce
hifiadi <hifiadi@gmail.com>
2021-09-10 19:11:06 -0500

    Disabled MongoDb

--- src/main/java/com/clover/urlshortner/UrlshortnerApplication.java
+++ src/main/java/com/clover/urlshortner/UrlshortnerApplication.java
@@ -2,8 +2,17 @@ package com.clover.urlshortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class,
+        MongoReactiveAutoConfiguration.class,
+        MongoReactiveDataAutoConfiguration.class,
+})
 public class UrlshortnerApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/clover/urlshortner/UrlshortnerApplicationTests.java
+++ src/test/java/com/clover/urlshortner/UrlshortnerApplicationTests.java
@@ -1,9 +1,13 @@
 package com.clover.urlshortner;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class UrlshortnerApplicationTests {
 
     @Test
